5aba90da45973b16ec426e29ece335e21a9ced11
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2015-01-03 15:11:39 +0100

    Push SSH keys to GitLab, run containers on web hooks

--- coduno.go
+++ coduno.go
@@ -2,17 +2,26 @@ package coduno
 
 import (
 	"appengine"
+	"appengine/urlfetch"
 	"crypto/rand"
 	"database/sql"
 	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"gitlab"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
+
+var gitlabToken = "YHQiqMx3qUfj8_FxpFe4"
+
+type ContainerCreation struct {
+	Id string `json:"Id"`
+}
 
 func connectDatabase() (*sql.DB, error) {
 	return sql.Open("mysql", "root@cloudsql(coduno:mysql)/coduno")
@@ -38,6 +47,53 @@ func push(w http.ResponseWriter, req *http.Request) {
 			context.Warningf(err.Error())
 		} else {
 			commit := push.Commits[0]
+
+			docker, _ := http.NewRequest("POST", "http://docker.cod.uno:2375/v1.15/containers/create", strings.NewReader("{\"Image\": \"ubuntu\", \"Cmd\": [\"date\"]}"))
+
+			docker.Header = map[string][]string{
+				"Content-Type": {"application/json"},
+				"Accept":       {"application/json"},
+			}
+
+			client := urlfetch.Client(context)
+			res, err := client.Do(docker)
+
+			if err != nil {
+				context.Debugf("Docker API response:", err.Error())
+				return
+			}
+
+			var result ContainerCreation
+			body, err := ioutil.ReadAll(res.Body)
+			err = json.Unmarshal(body, &result)
+
+			if err != nil {
+				context.Debugf("Received body %d: %s", res.StatusCode, string(body))
+				context.Debugf("Unmarshalling API response: %s", err.Error())
+				return
+			}
+
+			docker, _ = http.NewRequest("POST", "http://docker.cod.uno:2375/v1.15/containers/"+result.Id+"/start", nil)
+
+			docker.Header = map[string][]string{
+				"Content-Type": {"application/json"},
+				"Accept":       {"application/json"},
+			}
+
+			res, err = client.Do(docker)
+
+			if err != nil {
+				context.Debugf("Docker API response 2: %s", err.Error())
+			} else {
+				result, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					context.Debugf(err.Error())
+				} else {
+					context.Debugf(string(result))
+				}
+			}
+			defer res.Body.Close()
+
 			context.Infof("Received push from %s", commit.Author.Email)
 		}
 	}
@@ -56,11 +112,54 @@ func generateToken() (string, error) {
 func token(w http.ResponseWriter, req *http.Request) {
 	context := appengine.NewContext(req)
 
+	if req.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Invalid method.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := ioutil.ReadAll(req.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	if len(body) < 1 {
+		http.Error(w, "Invalid body.", http.StatusBadRequest)
+	}
+
 	if err, username := authenticate(req); err == nil {
 		token, err := generateToken()
 		if err != nil {
 			http.Error(w, "Failed to generate token.", http.StatusInternalServerError)
 		} else {
+			query := url.Values{}
+			query.Add("id", "2")
+			query.Add("title", username)
+			query.Add("key", string(body))
+
+			gitlabReq, _ := http.NewRequest("POST", "http://git.cod.uno/api/v3/users/2/keys", strings.NewReader(query.Encode()))
+
+			gitlabReq.Header = map[string][]string{
+				"PRIVATE-TOKEN": {gitlabToken},
+				"Content-Type":  {"application/x-www-form-urlencoded"},
+				"Accept":        {"application/json"},
+			}
+
+			client := urlfetch.Client(context)
+			res, err := client.Do(gitlabReq)
+			if err != nil {
+				context.Debugf(err.Error())
+			} else {
+				result, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					context.Debugf(err.Error())
+				} else {
+					context.Debugf(string(result))
+				}
+			}
+			defer res.Body.Close()
+
 			context.Infof("Generated token for '%s'", username)
 			fmt.Fprintf(w, token)
 		}
