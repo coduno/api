39362599b1d67dd3e56f53311c7db269d04eb826
Victor Balan <victor.balan@cod.uno>
2015-07-21 15:29:10 +0300

    serve dashboard and codeground from the same origin

---
+++ dirLinks.bat
@@ -0,0 +1,2 @@
+mklink /D %GOPATH%\src\github.com\coduno\app\dashboard %GOPATH%\src\github.com\coduno\dashboard
+mklink /D %GOPATH%\src\github.com\coduno\app\codeground %GOPATH%\src\github.com\coduno\codeground
