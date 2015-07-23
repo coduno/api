b855fb04a36abdc572de60dfe54aa87a6f2b14e9
pbochis <paul.bochis@catalysts.cc>
2015-07-23 13:36:20 +0300

    model, ctrl: Fingerprint
    Add controller to create Fingerprint and adapt routes.
    
    Also add Save(context.Context) to other models.

--- models/coder.go
+++ models/coder.go
@@ -1,9 +1,22 @@
 package models
 
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/appengine/datastore"
+)
+
+//CoderKind the name of the collection in datastore
+const CoderKind = "coders"
+
 // Coder contains the data related to a coder
 type Coder struct {
 	EntityID  int64  `datastore:"-" json:"id"`
 	Email     string `json:"email"`
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
+}
+
+//Save a new coder
+func (coder Coder) Save(ctx context.Context) (*datastore.Key, error) {
+	return datastore.Put(ctx, datastore.NewIncompleteKey(ctx, CoderKind, nil), &coder)
 }
