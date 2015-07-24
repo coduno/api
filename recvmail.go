111f0a3823c77619a586d24cabf8f709ce37318c
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2015-07-24 09:41:13 +0300

    mail: Make recvmail use delay and admin mail
    We can only send from an admin email-address.

--- recvmail.go
+++ recvmail.go
@@ -1,35 +1,61 @@
 package main
 
 import (
-	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/mail"
 
+	"golang.org/x/net/context"
+
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/delay"
 	"google.golang.org/appengine/log"
+	appmail "google.golang.org/appengine/mail"
 )
+
+var echoMailFunc = delay.Func("echoMail", echoMail)
+
+func echoMail(ctx context.Context, m mail.Message) {
+	from, err := m.Header.AddressList("From")
+	if err != nil {
+		log.Warningf(ctx, "Failed getting sender of mail: %+v", m)
+		return
+	}
+
+	b, _ := ioutil.ReadAll(m.Body)
+
+	am := &appmail.Message{
+		Sender:  "lorenz.leutgeb@cod.uno",
+		To:      []string{from[0].String()},
+		Body:    string(b),
+		Headers: m.Header,
+	}
+
+	err = appmail.Send(ctx, am)
+	if err != nil {
+		log.Errorf(ctx, err.Error())
+	}
+}
 
 func receiveMail(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 
 	m, err := mail.ReadMessage(r.Body)
 	if err != nil {
-		log.Warningf(ctx, "Failed reading an mail!")
+		log.Errorf(ctx, "Failed reading a mail!")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	from, err := m.Header.AddressList("From")
+	err = echoMailFunc.Call(ctx, m)
 	if err != nil {
-		log.Warningf(ctx, "Failed getting sender of mail: %+v", m)
+		log.Errorf(ctx, "Failed enqueing handler for a mail!")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	response := fmt.Sprintf("Received mail from %+v", from)
-	log.Debugf(appengine.NewContext(r), response)
-	io.WriteString(w, response)
+	io.WriteString(w, "OK")
 
 	// TODO(flowlo):
 	//  1. Check whether range m.Header.AddressList("From")
