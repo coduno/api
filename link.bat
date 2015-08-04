640699bb22130bb633afaf86a03696e7cd5a67c1
Lorenz Leutgeb <lorenz.leutgeb@cod.uno>
2015-08-04 11:58:46 +0300

    Remove linkers, as app is now a standalone module

--- link.bat
+++
@@ -1,2 +0,0 @@
-mklink /D %GOPATH%\src\github.com\coduno\app\dashboard %GOPATH%\src\github.com\coduno\dashboard
-mklink /D %GOPATH%\src\github.com\coduno\app\codeground %GOPATH%\src\github.com\coduno\codeground
