ad01370be9060c4c46f3529c54dc58c54ebe7cb9
Lorenz Leutgeb <lorenz.leutgeb@cod.uno>
2015-08-04 23:59:28 +0300

    ctrl: Rewrite certificate controller

--- controllers/certificate.go
+++ controllers/certificate.go
@@ -1,21 +1,25 @@
 package controllers
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 )
 
+// Certificate will return all public certificates assigned by App Engine in PEM format.
 func Certificate(ctx context.Context, w http.ResponseWriter, r *http.Request) (status int, err error) {
-	var c []appengine.Certificate
-	if c, err = appengine.PublicCertificates(ctx); err != nil {
+	var certs []appengine.Certificate
+	if certs, err = appengine.PublicCertificates(ctx); err != nil {
 		return http.StatusInternalServerError, err
 	}
 
-	if err = json.NewEncoder(w).Encode(c); err != nil {
-		return http.StatusInternalServerError, err
+	w.Header().Add("Content-Type", "application/x-pem-file")
+
+	for _, cert := range certs {
+		w.Write([]byte("KeyName: \"" + cert.KeyName + "\""))
+		w.Write(cert.Data)
 	}
+
 	return http.StatusOK, nil
 }
