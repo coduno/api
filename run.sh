1562764bef352a6311b7f4749555a69644d9423b
Lorenz Leutgeb <lorenz.leutgeb@cod.uno>
2015-10-16 15:30:37 +0200

    gae: Make run.sh bind to all interfaces by default

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-dev_appserver.py --port 8080 --admin_port 8000 --storage_path .storage --show_mail_body true $@ .
+dev_appserver.py --host 0.0.0.0 --port 8080 --admin_port 8000 --storage_path .storage --show_mail_body true $@ .
